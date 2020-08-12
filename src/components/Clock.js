744a9bcb489b9d9934b2349dd97c6901d20f4805
Jan Stümmel <info@janstuemmel.de>
2020-08-12 16:46:45 +0200

    fix, lower clock fontSize for low dpi screens
    * bump app version to 1.1.1
    * bump android versionCode to 5

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -6,10 +6,10 @@ import { secondsToMinutes } from '../util'
 export default function Clock(props) {
   return (
     <View style={styles.clock}>
-      <Text style={{ fontSize: 50, lineHeight: 50, marginBottom: 20 }}>
+      <Text style={{ fontSize: 50, lineHeight: 50 }}>
         { props.isPrep ? 'PREPARE' : props.isPause ? 'PAUSE' : 'WORK' }
       </Text>
-      <Text style={{ fontSize: 150, lineHeight: 150}}>
+      <Text style={{ fontSize: 130, lineHeight: 150}}>
         {props.isPrep ? props.time : secondsToMinutes(props.time)}
       </Text>
       <Text style={{ fontSize: 50, lineHeight: 50 }}>
